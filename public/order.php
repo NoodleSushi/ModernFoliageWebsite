9dd2776a84dd5b5b1cf579cdeffaa6ff2a903018
Caitlin Lindsay <21102036@usc.edu.ph>
2022-12-21 00:45:28 +0800

    Customer order and updated css

--- public/order.php
+++ public/order.php
@@ -8,54 +8,71 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/css/main.css">
     <link rel="stylesheet" href="../src/css/orderStyles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Modern Foliage</title>
 </head>
 <body>
     <header class="header-container">
-        <div class="header-content">
-            <h1 class="logo-text">Modern Foliage</h1>
-            <button class="cartButt"><img class="cart-button" src="img/shopping-cart.png" alt="" width="64px"></button> <!--to add the onclick function thing but idk yet-->
-        </div>
-    </header>
-    <article class="inventory-container">
-        <div class="inventory">
-            <div class="i-text">
-                <h2>ORDERS</h2>
-            </div>
-            
-            <div class="i-head">
-                <div class="i-search">
-                    <form method="post" action=''>
-                        <label for = "filter"> Sort By 
-                            <select id="filter" name="filter">
-                                <option value="">Select One</option>
-                                <option value="1">Pots and Plants</option>
-                                <option value="2">Pots Only</option>
-                                <option value="3">Plants Onlys</option>
-                            </select>
-                        </label>
-                        <label for="search">Filter
-                            <input id="search">
-                        </label>
-                    </form>
-                </div>
-              
-                <div class="table">
-                    <table>
-                        <tr>
-                            <th>OrderID</th>
-                            <th>Customer Name</th>
-                            <th>Delivery Mode</th>
-                            <th>Total Price</th>
-                            <th>Total Quantity</th>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #314529;">
+      <link rel="stylesheet" href="../src/css/orderStyles.css">
+        <a class="logo-text" href="home.php" style="color: white; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; font-size:x-large;">Modern Foliage</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent;">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active" style="color: white;">
+            </li>
+          </ul>
 
+          <a href=""><button name="btnfaq" class="btn btn-secondary btn-lg" onclick="faqClick(event)" style="margin-right: 2%; background-color: transparent; border: none;">FAQ</button></a>
+          <a href=""><button name="btncart" class="btn btn-secondary btn-lg" onclick="cartClick(event)" style="margin-right: 2%; background-color: transparent; border: none;">My Cart</button></a>
+          <a href="order.php"><button name="btnorder" class="btn btn-secondary btn-lg" onclick="ordersClick(event)" style="margin-right: 2%; background-color: transparent; border: none;">My Orders</button></a>
+          <button name="btnsignout" class="btn btn-secondary btn-lg" onclick="signoutClick(event)" style="background-color: transparent; border: none;">Sign Out</button>
+          
         </div>
-        
-    </article>
+      </nav>
+    </header>
+    <main>
+        <div class="card-cont">
+            <div class="orderCard">
+                <div class="one">
+                    <div class="date"><p class="p">Sample Date</p></div>
+                    <div class="products"><p class="p">Sample Products</p></div>
+                    
+                </div>
+                <div class="two">
+                    <div class="cost"><p class="p">Sample Cost</p></div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="card-cont">
+            <div class="orderCard completed">
+                    <div class="one">
+                    <div class="date"><p class="p">Sample Date</p></div>
+                    <div class="products"><p class="p">Sample Products</p></div>
+                    
+                </div>
+                <div class="two">
+                    <div class="cost"><p class="p">Sample Cost</p></div>
+                </div>
+            </div>
+        </div>
+        <div class="card-cont">
+            <div class="orderCard completed">
+            <div class="one">
+                    <div class="date"><p class="p">Sample Date</p></div>
+                    <div class="products"><p class="p">Sample Products</p></div>
+                    
+                </div>
+                <div class="two">
+                    <div class="cost"><p class="p">Sample Cost</p></div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
