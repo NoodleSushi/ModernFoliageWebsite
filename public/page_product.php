a4df40cfcec43260cd47c7132889a8385443a03e
Caitlin Lindsay <21102036@usc.edu.ph>
2022-12-21 02:54:38 +0800

    Indivial, All Products

--- public/page_product.php
+++ public/page_product.php
@@ -8,16 +8,31 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../src/css/main.css">
     <link rel="stylesheet" href="../src/css/page_product.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>Product Page</title>
 </head>
 <body>
-    <header class="header-container">
-        <div class="header-content">
-            <h1 class="logo-text">Modern Foliage</h1>
-            <img class="cart-button" src="img/shopping-cart.png" alt="" width="64px">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #314529;">
+        <a class="logo-text" href="home.php" style="color: white; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; font-size:x-large;">Modern Foliage</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent;">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active" style="color: white;">
+            </li>
+          </ul>
+
+          <a href="faq.php"><button name="btnfaq" class="btn btn-secondary btn-lg"  style="margin-right: 2%; background-color: transparent; border: none;">FAQ</button></a>
+          <a href="cart.php"><button name="btncart" class="btn btn-secondary btn-lg" style="margin-right: 2%; background-color: transparent; border: none;">My Cart</button></a>
+          <a href="order.php"><button name="btnorder" class="btn btn-secondary btn-lg"  style="margin-right: 2%; background-color: transparent; border: none;">My Orders</button></a>
+          <button name="btnsignout" class="btn btn-secondary btn-lg" onclick="signoutClick(event)" style="background-color: transparent; border: none;">Sign Out</button>
+          
         </div>
-    </header>
+      </nav>
     <div class = "main_container">
         <div class="column">
             <img class="img_container" src="../img/plant.png">
